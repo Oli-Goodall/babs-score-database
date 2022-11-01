c3e9f92c6ff0912188c460bfd751c9a68645e542
Oli Goodall <Oli.Goodall@softwire.com>
2022-11-01 17:47:16 +0000

    Started work on frontend

--- frontend/src/components/contest/Contest.tsx
+++ frontend/src/components/contest/Contest.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 export const Contest: React.FunctionComponent = () => {
+    // const 
     return(
-        <></>
+        <>
+            <h1>Select a contest:</h1>
+            <div className="contest-list-page">
+                
+            </div>
+        </>
     );
 };
